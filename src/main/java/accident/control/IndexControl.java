87102c2c9fe83ae7e82b870e8a9865fcaf5f13c2
ogneyar79 <maksimus.s@mail.ru>
2021-03-28 11:55:07 +0300

    Initial commit accident

---
+++ src/main/java/accident/control/IndexControl.java
@@ -0,0 +1,16 @@
+package accident.control;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexControl {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+    
+}
